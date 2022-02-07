258a1f49fa64679fcc1c011422ba176f315bd1a1
shohel.rana <shohel.rana@mymedicalhub.com>
2022-02-07 21:00:27 +0600

    Initial Commit

---
+++ CameraSwiftUI/CameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSwiftUIApp.swift
+//  CameraSwiftUI
+//
+//  Created by Khayrul on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraSwiftUI
+//
+//  Created by Khayrul on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
